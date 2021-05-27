f07ac8ba98be7d06d5e88a9910b0677630af0d88
mertyilmazz <m3rtylmz@gmail.com>
2021-05-27 16:04:30 +0200

    ShoppingApi

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Data;
+using Shopping.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+        //private readonly ProductContext _context;
+
+        public ProductController(/*ProductContext context,*/ ILogger<ProductController> logger)
+        {
+            // _context = context ?? throw new ArgumentNullException(nameof(context));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return ProductContext.Products;
+        }
+    }
+}
