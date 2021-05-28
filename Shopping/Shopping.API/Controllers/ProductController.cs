a07dccce449fb82a1ca0b450c635ea269c560bb2
mertyilmazz <m3rtylmz@gmail.com>
2021-05-28 15:49:22 +0200

    Docker Part

--- Shopping/Shopping.API/Controllers/ProductController.cs
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 using Shopping.API.Data;
 using Shopping.API.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Shopping.API.Controllers
@@ -14,18 +14,18 @@ namespace Shopping.API.Controllers
     public class ProductController : Controller
     {
         private readonly ILogger<ProductController> _logger;
-        //private readonly ProductContext _context;
+        private readonly ProductContext _context;
 
-        public ProductController(/*ProductContext context,*/ ILogger<ProductController> logger)
+        public ProductController(ProductContext context, ILogger<ProductController> logger)
         {
-            // _context = context ?? throw new ArgumentNullException(nameof(context));
+            _context = context ?? throw new ArgumentNullException(nameof(context));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         [HttpGet]
         public async Task<IEnumerable<Product>> Get()
-        {
-            return ProductContext.Products;
+        {           
+            return await _context.Products.Find(p => true).ToListAsync();
         }
     }
 }
